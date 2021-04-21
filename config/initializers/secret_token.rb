7f1b31f901108ec5118e3db1022c5a7864691882
ShubhamK1551 <rockshubham1551@gmail.com>
2021-04-21 11:47:18 +0530

    Initial  commit of the Project1.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Project1::Application.config.secret_token = '0f5c2e225acae3348e8cd05a827b958be87d8b0dda0cf53804d27821c0e76734a47a2e57009b1096c99909d6072b3903de656fc79f11af26f17a44b0ada62564'
